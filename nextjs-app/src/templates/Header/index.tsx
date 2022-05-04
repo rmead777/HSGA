3c4e4f34dc0e37ae6d8e1b8659935be733fb35f4
drüdrükung-fu <5908477-drewdrewthis@users.noreply.gitlab.com>
2022-05-04 11:55:28 +0000

    Create Settings Page

--- nextjs-app/src/templates/Header/index.tsx
+++ nextjs-app/src/templates/Header/index.tsx
@@ -1,10 +1,33 @@
+import Image from "../../components/atoms/Image";
 import Link from "../../components/atoms/Link";
+import RoutesService from "../../services/RoutesService";
+import cx from "classnames";
+import fonts from "@ui/styles/fonts.module.css";
 
 export default function Footer() {
   return (
-    <header className="flex justify-between container p-3 max-w-7xl mx-auto font-bold text-lg uppercase">
-      <Link href="/">Home</Link>
-      <span>Logout</span>
+    <header
+      className={cx(
+        fonts.header,
+        "flex justify-between container p-3 max-w-7xl mx-auto font-bold text-lg uppercase"
+      )}
+    >
+      <Link className="chubby-choo-vertical-fix" href="/">
+        Home
+      </Link>
+      <span className="flex space-x-2">
+        <Link href="/logout" className="chubby-choo-vertical-fix">
+          Logout
+        </Link>
+        <Link href="/settings">
+          <Image
+            src={RoutesService.createAssetsPath("icons/icon-gear.svg")}
+            alt="twitter-icon"
+            width={24}
+            height={24}
+          />
+        </Link>
+      </span>
     </header>
   );
 }

dbf2ab433998acb25b5aa88346e63a1a8ad3debf
drüdrükung-fu <5908477-drewdrewthis@users.noreply.gitlab.com>
2022-05-05 18:56:39 +0000

    User login form

--- nextjs-app/src/components/organisms/forms/PasswordInput/index.tsx
+++ nextjs-app/src/components/organisms/forms/PasswordInput/index.tsx
@@ -1,6 +1,8 @@
-import { DetailedHTMLProps, InputHTMLAttributes } from "react";
+import { DetailedHTMLProps, InputHTMLAttributes, useState } from "react";
 import styles from "./styles.module.css";
 import TextInput from "@ui/organisms/forms/TextInput";
+import Image from "../../../atoms/Image";
+import RoutesService from "../../../../services/RoutesService";
 
 interface PropTypes
   extends DetailedHTMLProps<
@@ -12,20 +14,38 @@ interface PropTypes
 }
 
 function PasswordInput(props: PropTypes) {
+  const [showPassword, setShowPassword] = useState(false);
   const { hideRules, label = "Password", ...rest } = props;
+
+  const iconSrc = RoutesService.getIconPath(
+    showPassword ? "eye-open" : "eye-closed"
+  );
 
   return (
     <>
-      <TextInput
-        type="password"
-        name="password"
-        required
-        id="password"
-        aria-required="true"
-        label={label}
-        minLength={8}
-        {...rest}
-      />
+      <div className={styles["input-wrapper"]}>
+        <TextInput
+          name="password"
+          required
+          id="password"
+          aria-required="true"
+          label={label}
+          minLength={8}
+          {...{
+            ...rest,
+            type: !showPassword ? "password" : undefined,
+          }}
+        />
+        <Image
+          className={styles.icon}
+          src={iconSrc}
+          alt="featured-image"
+          width={24}
+          height={24}
+          zoom={1}
+          onClick={() => setShowPassword(!showPassword)}
+        />
+      </div>
       {!hideRules && (
         <ul className={styles.rules}>
           <li>Must have 8 characters minimum</li>

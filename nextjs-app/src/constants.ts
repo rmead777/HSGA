f9c05b14e90d735186645dca9905c9be3637bf3f
drewdrew <drewdrewthis@gmail.com>
2022-05-16 19:05:27 +0200

    Update discord link

--- nextjs-app/src/constants.ts
+++ nextjs-app/src/constants.ts
@@ -3,7 +3,7 @@ export const HOST = "https://testing2.ttechr.com/";
 export const EXTERNAL_LINKS = {
   twitter: "https://twitter.com/HighScoreWM",
   instagram: "https://www.instagram.com/highscorewm/",
-  discord: "https://discord.gg/CtjkeXSb",
+  discord: "https://discord.gg/tkP6mWkMt7",
 };
 
 export const IMGIX_HOST = "https://hswm.imgix.net/";
